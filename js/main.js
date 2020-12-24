23986b5ade44af6a7f51e272f63fce1ce1156239
arcangelo7 <arcangelomas@gmail.com>
2020-12-24 13:33:31 +0100

    slider, scale, round values

--- js/main.js
+++ js/main.js
@@ -6,22 +6,20 @@ var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
 
+var selectedYear = 2011;
+
 // Map and projection
 var path = d3.geoPath(); // Maps GeoJSON features to SVG
 var projection = d3.geoEckert6()
   .scale(200)
   .translate([width / 2, height / 2]);
 
-// Data and color scale
 var labour_force = new Map(); // The Map object holds key-value pairs and remembers the original insertion order of the keys.
-var colorScale = d3.scaleThreshold()
-  .domain([0, 10, 20, 30, 40, 50, 60]) 
-  .range(d3.schemeBlues[7]);
 
-// Load external data and boot
+// Load external data 
 var promises = [
     d3.json("data/world.geojson"),
-    d3.csv("data/labour_force.csv", function(d) { labour_force.set(d.country_code, +d.YR2020); })
+    d3.csv("data/labour_force.csv", function(d) { labour_force.set(d.country_code, {1990:+d.YR1990, 2000:+d.YR2000, 2011:+d.YR2011, 2012:+d.YR2012, 2013:+d.YR2013, 2014:+d.YR2014, 2015:+d.YR2015, 2016:+d.YR2016, 2017:+d.YR2017, 2018:+d.YR2018, 2019:+d.YR2019, 2020:+d.YR2020}); })
 ]
 
 Promise.all(promises).then(function(data){
@@ -30,7 +28,16 @@ Promise.all(promises).then(function(data){
     console.log(error);
 });
 
-function ready(world) {
+function ready(world) {    
+    var colorScale = d3.scaleThreshold()
+        .domain([0, 10, 20, 30, 40, 50, 60]) 
+        .range(d3.schemeBlues[7]);
+  
+    // Legend scale
+    var x = d3.scaleLinear()
+        .domain([0, 60])
+        .rangeRound([700, 990]);
+
     let mouseOver = function(event, d) {
         d3.selectAll(".Country")
           .transition()
@@ -45,7 +52,7 @@ function ready(world) {
         tip.transition()
           .duration(200)
           .style("opacity", .9);
-        tip.html(d.properties.name + "<br/>" + d.labour_force + "%")
+        tip.html(d.properties.name + "<br/>" + (Math.round(d.labour_force[selectedYear]) || "No") + "%")
             .style("left", (event.pageX) + "px")
             .style("top", (event.pageY - 28) + "px");
     }
@@ -64,6 +71,7 @@ function ready(world) {
             .duration(500)
             .style("opacity", 0);
     }
+
     // Draw the map
     svg.append("g")
         .selectAll("path")
@@ -79,14 +87,72 @@ function ready(world) {
         // set the color of each country
         .attr("fill", function (d) {
             d.labour_force = labour_force.get(d.id) || 0;
-            return colorScale(d.labour_force);
         })
         .style("stroke", "black")
         .attr("class", function(d){ return "Country" } )
         .style("opacity", 1)
         .on("mouseover", mouseOver )
         .on("mouseleave", mouseLeave )
+    
+    // Legend 
+    var legend = svg.append("g")
+        .attr("class", "key")
+        .attr("transform", "translate(0,15)");
+
+    legend.selectAll("rect")
+        .data(colorScale.range().map(function(d) {
+            d = colorScale.invertExtent(d);
+            if (d[0] == null) d[0] = x.domain()[0];
+            if (d[1] == null) d[1] = x.domain()[1];
+            return d;
+        }))
+        .enter().append("rect")
+            .attr("height", 8)
+            .attr("x", function(d) { return x(d[0]); })
+            .attr("width", function(d) { return x(d[1]) - x(d[0]); })
+            .attr("fill", function(d) { return colorScale(d[0]); });
+
+    legend.append("text")
+        .attr("class", "caption")
+        .attr("x", x.range()[0])
+        .attr("y", -6)
+        .attr("fill", "#000")
+        .attr("text-anchor", "start")
+        .attr("font-weight", "bold")
+        .text("Female labor force rate");
+
+    legend.call(d3.axisBottom(x)
+        .tickSize(13)
+        .tickFormat(function(x, i) { return i ? x : x + "%"; })
+        .tickValues(colorScale.domain()))
+      .select(".domain")
+        .remove();
+    
+    // Slider
+    function update(year){
+        slider.property("value", year);
+        d3.select(".year").text(year);
+        d3.selectAll(".Country")
+            .style("fill", function(d) {
+                return colorScale(d.labour_force[year]);
+            });
+
+        selectedYear = year;
+    }
+
+    var slider = d3.select(".slider")
+		.append("input")
+			.attr("type", "range")
+			.attr("min", 2011)
+			.attr("max", 2020)
+			.attr("step", 1)
+			.on("input", function() {
+				var year = this.value;
+				update(year);
+            });
+    update(selectedYear);
 }
+
 
 
 
