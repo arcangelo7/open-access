e2e7c94512e9ecbe56a2b1a80d1d476b91a4d6f5
arcangelo7 <arcangelomas@gmail.com>
2020-12-26 15:32:26 +0100

    temp datasets

--- python/labour.py
+++ python/labour.py
@@ -1,6 +1,6 @@
 import csv, itertools, re, pandas
 
-with open("data/labour_force.csv") as csvfile:
+with open("data/temp/labour_force.csv") as csvfile:
     labour_force = list(csv.DictReader(csvfile))
     labour_force_new = list()
     list_of_years = list()
@@ -10,12 +10,12 @@ with open("data/labour_force.csv") as csvfile:
     for values in itertools.product(labour_force, list_of_years):
         labour_force_new.append({"country_name": values[0]["country_name"], "country_code": values[0]["country_code"], "year": re.sub("YR", "", values[1]), "female_labour_force": values[0][values[1]]})
 
-# with open("data/labour_force_new.csv", "w", newline="") as csvfile:
-#     dict_writer = csv.DictWriter(csvfile, labour_force_new[0].keys())
-#     dict_writer.writeheader()
-#     dict_writer.writerows(labour_force_new)
+with open("data/temp/labour_force_new.csv", "w", newline="") as csvfile:
+    dict_writer = csv.DictWriter(csvfile, labour_force_new[0].keys())
+    dict_writer.writeheader()
+    dict_writer.writerows(labour_force_new)
 
-with open("data/marital_status.csv") as csvfile:
+with open("data/temp/marital_status.csv") as csvfile:
     marital_status_new = list()
     marital_status = list(csv.DictReader(csvfile))
     dict_of_years = dict()
@@ -29,8 +29,15 @@ with open("data/marital_status.csv") as csvfile:
         dict_of_years.setdefault(row["source.label"][0:3], {row["time"]})
         dict_of_years[row["source.label"][0:3]].add(row["time"])
 
-a = pandas.read_csv("data/labour_force_new.csv")
-b = pandas.read_csv("data/marital_status_new.csv")
+keys = ["country_name", "country_code", "year", "total", "single", "married", "union_cohabiting", "widowed", "divorced", "single_widowed_divorced", "married_union_cohabiting", "not_elsewhere_classified"]
+
+with open("data/temp/marital_status_new.csv", "w", newline="") as csvfile:
+    dict_writer = csv.DictWriter(csvfile, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(marital_status_new)
+
+a = pandas.read_csv("data/temp/labour_force_new.csv")
+b = pandas.read_csv("data/temp/marital_status_new.csv")
 merged = a.merge(b, on=["country_name", "country_code", "year"], how="outer")
 merged.to_csv("data/labour.csv", index=False)
 
