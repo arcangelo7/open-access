46655e9f56ebf13e5b61e22f660cc0ca218a747a
Arcangelo <info@arcangelomassari.com>
2021-01-06 20:09:49 +0100

    linee e focus

--- js/education.js
+++ js/education.js
@@ -7,7 +7,7 @@ var marginEdu = { left:80, right:100, top:50, bottom:100 },
 var svgEdu = d3.select("#eduChart")
   .attr("width", widthEdu + marginEdu.left + marginEdu.right)
   .attr("height", heightEdu + marginEdu.top + marginEdu.bottom)
-  .append("g")
+var gEdu = svgEdu.append("g")
   .attr("transform", "translate(" + marginEdu.left + "," + marginEdu.top + ")");
 
 // parse the date / time
@@ -15,6 +15,14 @@ var parseTimeEdu = d3.timeParse("%Y");
 var formatTime = d3.timeFormat("%Y");
 // For tooltip
 var bisectDate = d3.bisector(function(d) { return d.year; }).left;
+
+// Add the line for the first time
+gEdu.append("path")
+    .attr("class", "line")
+    .attr("fill", "none")
+    .attr("stroke", "grey")
+    .attr("stroke-width", "3px");
+
 // Scales
 var xEdu = d3.scaleTime().range([0, widthEdu]);
 var yEdu = d3.scaleLinear().range([heightEdu, 0]);
@@ -27,11 +35,11 @@ var yAxisCallEdu = d3.axisLeft().scale(yEdu)
     .tickFormat(function(d) { return parseInt(d) + "%"; });
 
 // Place the axes on the chart
-var xAxisEdu = svgEdu
+var xAxisEdu = gEdu
     .append("g")
     .attr("class", "x-axis")
     .attr("transform", "translate(0," + heightEdu + ")")
-var yAxisEdu = svgEdu
+var yAxisEdu = gEdu
     .append("g")
     .attr("class", "y-axis")
 // Y-Axis label
@@ -45,9 +53,9 @@ var yAxisEdu = svgEdu
 //     .text("Female percentage");
 
 // Line path generator
-var line = d3.line()
-    .x(function(d) { return x(d.year); })
-    .y(function(d) { return y(d.value); });
+var lineEdu = d3.line()
+    .x(function(d) { return xEdu(d.year); })
+    .y(function(d) { return yEdu(d.value); });
 
 // Event listeners
 $("#eduMeasure").on("change", updateEdu)
@@ -104,6 +112,59 @@ function updateEdu(){
     xAxisEdu.transition(t()).call(xAxisCallEdu);
     yAxisCallEdu.scale(yEdu);
     yAxisEdu.transition(t()).call(yAxisCallEdu);
+
+    // Path generator
+    lineEdu = d3.line()
+        .x(function(d){ return xEdu(d.year); })
+        .y(function(d){ return yEdu(d.value); });
+    // Update our line path
+    gEdu.select(".line")
+        .transition(t)
+        .attr("d", lineEdu(dataTimeFiltered));
+
+    // Clear old tooltips
+    d3.select(".focus").remove();
+    d3.select(".overlay").remove();
+    
+    // Tooltip code
+    var focus = gEdu.append("g")
+        .attr("class", "focus")
+        .style("display", "none");
+    focus.append("line")
+        .attr("class", "x-hover-line hover-line")
+        .attr("y1", 0)
+        .attr("y2", heightEdu);
+    focus.append("line")
+        .attr("class", "y-hover-line hover-line")
+        .attr("x1", 0)
+        .attr("x2", widthEdu);
+    focus.append("circle")
+        .attr("r", 5);
+    focus.append("text")
+        .attr("x", 15)
+        .attr("dy", ".31em");
+    svgEdu.append("rect")
+        .attr("transform", "translate(" + marginEdu.left + "," + marginEdu.top + ")")
+        .attr("class", "overlay")
+        .attr("width", widthEdu)
+        .attr("height", heightEdu)
+        .on("mouseover", function() { focus.style("display", null); })
+        .on("mouseout", function() { focus.style("display", "none"); })
+        .on("mousemove", mousemoveEdu);
+
+    function mousemoveEdu(event) {
+        var x0edu = xEdu.invert(d3.pointer(event)[0]),
+            i = bisectDate(dataTimeFiltered, x0edu, 1),
+            d0 = dataTimeFiltered[i - 1],
+            d1 = dataTimeFiltered[i],
+            d = (d1 && d0) ? (x0edu - d0.date > d1.date - x0edu ? d1 : d0) : 0;
+        focus.attr("transform", "translate(" + xEdu(d.year) + "," + yEdu(d.value) + ")");
+        focus.select("text").text(function() { 
+            return Math.round(d.value) + "%"; 
+        });
+        focus.select(".x-hover-line").attr("y2", heightEdu - yEdu(d.value));
+        focus.select(".y-hover-line").attr("x2", -xEdu(d.year));
+    }
 }
 
 
